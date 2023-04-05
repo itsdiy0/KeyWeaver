0fa786874b6b6cddd6c1cad7a2f991615a5c1493
Student <student@C17775949.offcampusnetwork.co.uk>
2023-04-05 18:27:19 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function randomChars(){
+    
+}
+
+
+let main_input = document.getElementById("mainInput");
+let generate_button = document.getElementById("generateButton");
+generate_button.onclick = function(){
+    console.log(main_input.value);
+};
